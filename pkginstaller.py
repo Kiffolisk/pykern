124f6dd109f4850821e1f9caa4ba0a9025c2c54d
Arezalgamer89 <86628069+ArezalGame89@users.noreply.github.com>
2021-12-06 01:05:22 +0330

    Create pkginstaller.py
    Using this for packages python needs

---
+++ pkginstaller.py
@@ -0,0 +1,23 @@
+os.system('color 0a' if os.name=='nt' else '') #making the text green because it's cool B)
+#some people wouldn't have the packages,
+#this is a low-effort attempt to install the modules that were used
+#altough it may not just work
+#please correct any issues or misses i made
+
+pip install urllib3 #in this case i used 3 because none other would work
+#pip install urllib    try for yourself
+#pip install urllib2   it wants urllib3 for satisfaction
+
+print('RE should be installed already in python')
+
+#usually, you can also use RXPY or nsre. but i'd not expect that
+#now, you also should have os built-in, but i may be dumb af so
+#importing OS if neccesary
+import os
+from os import path
+#I'm a python 2 fan. i usually don't even know if pykern is built for py3 or not
+
+#some main.py stuff
+pip install colorama 
+pip install importlib
+print('installed the main.py libs?')
