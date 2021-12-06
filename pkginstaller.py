ece7c9564900f9404f97b0657104ad554ca8b3a9
NitrogenDioxide <45130240+Kiffolisk@users.noreply.github.com>
2021-12-06 13:17:45 +0100

    fix

--- pkginstaller.py
+++ pkginstaller.py
@@ -2,7 +2,7 @@ import os
 
 os.system('color 0a' if os.name=='nt' else '')
 
-os.system("pip install urllib3") #in this case i used 3 because none other would work
+os.system("pip install urllib") #in this case i used 3 because none other would work
 #pip install urllib    try for yourself
 #pip install urllib2   it wants urllib3 for satisfaction
 
