82b3e0cc99e335ae0d519e486f4b188f4ab3cd57
NitrogenDioxide <45130240+Kiffolisk@users.noreply.github.com>
2021-12-07 10:14:16 +0100

    Update pkginstaller.py

--- pkginstaller.py
+++ pkginstaller.py
@@ -2,10 +2,6 @@ import os
 
 os.system('color 0a' if os.name=='nt' else '')
 
-os.system("pip install urllib") #in this case i used 3 because none other would work
-#pip install urllib    try for yourself
-#pip install urllib2   it wants urllib3 for satisfaction
-
 #some main.py stuff
 os.system("pip install colorama") 
 os.system("pip install importlib") 
